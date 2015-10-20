96cf3e6ace71f207c2a19d791558684545501c2e
Bartosz Przybylski <bart.p.pl@gmail.com>
2015-10-20 23:01:08 +0200

    Add swap storages feature

--- chrome/options.js
+++ chrome/options.js
@@ -25,10 +25,34 @@ function restore_options() {
   }
 }
 
+function swap_storage() {
+  chrome.storage.local.get('clipboard', function(localItems) {
+    if (!(localItems.clipboard instanceof Array))
+      localItems.clipboard = new Array();
+
+    chrome.storage.sync.get('clipboard', function(syncItems) {
+      if (!(syncItems.clipboard instanceof Array))
+        syncItems.clipboard = new Array();
+      
+      chrome.storage.sync.set({'clipboard': localItems.clipboard}, function() {
+        if (chrome.runtime.lastError) {
+          alert("Setting to synchronized storage failed, aborting: " + chrome.runtime.lastError.message);
+          return;
+        }
+        chrome.storage.local.set({'clipboard': syncItems.clipboard});
+        chrome.runtime.sendMessage({event:'rebuildMenus'});
+      });
+    });
+
+  });
+}
+
 function init_i18n() {
   document.getElementById("title").innerHTML = chrome.i18n.getMessage("optionsText");
   document.getElementById("option_sync_type_text").innerHTML = chrome.i18n.getMessage("optionStorageTypeText");
   document.getElementById("option_sync_text").innerHTML = chrome.i18n.getMessage("optionSyncText");
+  document.getElementById("option_swap_storages_text").innerHTML = chrome.i18n.getMessage("optionSwapStorageText");
+  document.getElementById("option_swap_storage_btn_text").innerText = chrome.i18n.getMessage("optionSwapText");
   document.getElementById("option_local_text").innerHTML = chrome.i18n.getMessage("optionLocalText");
   document.getElementById("option_sync_tip").innerText = chrome.i18n.getMessage("optionSyncTip");
   document.getElementById("save").innerText = chrome.i18n.getMessage("optionsSave");
@@ -41,3 +65,4 @@ function dom_loaded() {
 
 document.addEventListener('DOMContentLoaded', dom_loaded);
 document.querySelector("#save").addEventListener('click', save_options);
+document.querySelector('#option_swap_storage_btn_text').addEventListener('click', swap_storage);
