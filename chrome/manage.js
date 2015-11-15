19a1cebee318a014c59e6f81e31a78fb3cde80fb
Bartosz Przybylski <bart.p.pl@gmail.com>
2015-11-16 00:41:24 +0100

    Browse entire clipboard structure

--- chrome/manage.js
+++ chrome/manage.js
@@ -46,7 +46,9 @@ function displayClipboardElements() {
     $('#browser_wrapper').append(toplevel);
 
     for (var i = 0; i < items.clipboard.length; ++i) {
-      toplevel.append(createEntryViewForData(items.clipboard[i]));
+      var ee = createEntryViewForData(items.clipboard[i]);
+      toplevel.append(ee);
+      ee.attr('path', '_'+i);
     }
 
     toplevel.sortable({
@@ -68,18 +70,65 @@ function initiateLiveEvents() {
       currentlySelected.removeClass('ui-selected');
     }
     $(event.target).addClass('ui-selected');
-    tearDownBrowserIfNeeded();
-    currentlySelected = $(event.target);
-    appendBrowserIfNeeded();
+    var incoming = $(event.target);
+    var path = tearDownBrowserIfNeeded(incoming);
+    currentlySelected = incoming;
+    appendBrowserIfNeeded(incoming, path);
   });
 }
 
-function tearDownBrowserIfNeeded() {
+function tearDownBrowserIfNeeded(incoming) {
+  if (currentlySelected == null)
+    return incoming.attr('path').split("_");
+  var incomingPath = incoming.attr('path').split("_");
+  var currentPath = currentlySelected.attr('path').split("_");
 
+  var same = 0;
+
+  for (var i = 0; i < Math.min(incomingPath.length, currentPath.length); ++i) {
+    if (incomingPath[i] != currentPath[i]) break;
+    same++;
+  }
+  
+  if (same == incomingPath.length) same--;
+
+  var e = $('#browser_wrapper .browser_window');
+  for (var i = same; i < e.length; ++i) {
+    e[i].remove();
+  }
+
+  return incomingPath;
 }
 
-function appendBrowserIfNeeded() {
+function appendBrowserIfNeeded(incoming, pathLeft) {
 
+  getStorage().get('clipboard', function(items) {
+
+
+    if (currentlySelected != null && currentlySelected.hasClass('submenu')) {
+      var displayElems = items.clipboard;
+      for (var i = 1; i < pathLeft.length; ++i) {
+        displayElems = displayElems[parseInt(pathLeft[i])].value;
+      }
+
+      var level = createEntriesWindow();
+
+      $('#browser_wrapper').append(level);
+      for (var i = 0; i < displayElems.length; ++i) {
+        var ee = createEntryViewForData(displayElems[i]);
+        level.append(ee);
+        ee.attr('path', currentlySelected.attr('path')+'_'+i);
+      }
+      level.sortable({
+        placeholder: "list-placeholder",
+        forcePlaceholderSize: true,
+        cursor: "ns-resize",
+        axis: 'y',
+        revert: 100,
+      });
+    }
+
+  });
 }
 
 var currentlySelected;
