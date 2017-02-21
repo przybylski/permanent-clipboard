be7df1486221c00705c065feedb461a1bf7b8b59
Bartosz Przybylski <bart.p.pl@gmail.com>
2017-02-22 00:09:58 +0100

    Use switch instead of if-else cascade

--- src/contentscript.js
+++ src/contentscript.js
@@ -1,19 +1,22 @@
-function onCopy(e) {
-  var selection = document.getSelection();
-  if (selection.toString().length > 0)
-    chrome.runtime.sendMessage({event:"saveRecentItem", value: selection.toString()});
-}
-
 chrome.runtime.onMessage.addListener(
   function(message, sender, sendResponse) {
-    if (message.event == "insertText") {
-      document.execCommand("insertText", true, message.value);
-    } else if (message.event == "addSelection") {
-      var selection = document.getSelection().toString();
-      if (selection.length > 0)
-        chrome.runtime.sendMessage({event: 'addNewEntry', value: selection});
+    switch (message.event) {
+      case 'insertText':
+        document.execCommand("insertText", true, message.value);
+        break;
+      case 'addSelection':
+        var selection = document.getSelection().toString();
+        if (selection.length > 0)
+          chrome.runtime.sendMessage({event: 'addNewEntry', value: selection});
+        break;
+      default:
+        console.warn('Unknown message event sent to content script' + message.event);
     }
     sendResponse({});
   });
 
-document.addEventListener('copy', onCopy, true);
+document.addEventListener('copy', function(e) {
+  var selection = document.getSelection().toString();
+  if (selection.length > 0)
+    chrome.runtime.sendMessage({event:"saveRecentItem", value: selection});
+});
