8b7a4e647c8341ac42ed65047993f2bb124f64c0
Bartosz Przybylski <bart.p.pl@gmail.com>
2017-02-21 23:14:13 +0100

    Move analytics to new way of gathering events

--- src/analytics.js
+++ src/analytics.js
@@ -1,25 +1,20 @@
 function Analytics() {}
 
 Analytics.prototype = {
-	userCode: function() { return 'UA-64085295-1'},
+	userCode: function() { return 'UA-64085295-1'; },
 	initialize: function() {
-		_gaq.push(['_setAccount', this.userCode()]);
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://ssl.google-analytics.com/analytics.js','ga');
+
+		ga('create', this.userCode(), 'auto');
+		ga('set', 'checkProtocolTask', null);
 	},
-	appendToDocument: function() {
-		var ga = document.createElement('script');
-		ga.type = 'text/javascript';
-		ga.async = true;
-		ga.src = 'https://ssl.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0];
-		s.parentNode.insertBefore(ga, s);
-	},
-	trackEvent: function(category, subcategory) {
-		_gaq.push(['_trackEvent', category, subcategory]);
+	trackEvent: function(category, action) {
+		ga('send', 'event', category, action);
 	}
 }
 
-var _gaq = _gaq || [];
-
 var analytics = new Analytics();
 analytics.initialize();
-analytics.appendToDocument();
