7824133a30a6acaafadeef21ff1dc9b3eafac3c0
Bartosz Przybylski <bart.p.pl@gmail.com>
2017-02-26 23:01:35 +0100

    Improve looks of options tab with materialize css framework

--- src/options.js
+++ src/options.js
@@ -5,12 +5,8 @@ function save_options() {
   var storage_type = select.children[select.selectedIndex].value;
   localStorage["storage_type"] = storage_type;
   chrome.runtime.sendMessage({event:'rebuildMenus'});
-  
-  var status = document.getElementById("status");
-  status.innerHTML = "Options saved.";
-  setTimeout(function() {
-    status.innerHTML = "";
-  }, 750);
+
+  Materialize.toast(chrome.i18n.getMessage('optionsSavedToastText'), 2000);
 }
 
 function restore_options() {
@@ -30,45 +26,53 @@ function restore_options() {
 
 function swap_storage() {
   chrome.storage.local.get('clipboard', function(localItems) {
-    if (!(localItems.clipboard instanceof Array))
-      localItems.clipboard = new Array();
+    localItems.clipboard = localItems.clipboard || [];
 
     chrome.storage.sync.get('clipboard', function(syncItems) {
-      if (!(syncItems.clipboard instanceof Array))
-        syncItems.clipboard = new Array();
+      syncItems.clipboard = syncItems.clipboard || []; 
 
       chrome.storage.sync.set({'clipboard': localItems.clipboard}, function() {
         if (chrome.runtime.lastError) {
-          alert("Setting to synchronized storage failed, aborting: " + chrome.runtime.lastError.message);
+          Materialize.toast(chrome.i18n.getMessage('storageSwappingFailedMessage') + chrome.runtime.lastError.message, 4000);
           return;
         }
         chrome.storage.local.set({'clipboard': syncItems.clipboard});
         chrome.runtime.sendMessage({event:'rebuildMenus'});
+        
+        Materialize.toast("Success", 4000);
+        calculate_and_set_fill_bar();
+
       });
     });
 
   });
 }
 
-function get_storage_fill_color(percent) {
+function getStorageColorClass(percent) {
   if (typeof(percent) != typeof(0.1))
-    return "#000000";
+    return "black";
   if (percent < 0 || percent > 1)
-    return "#0000FF";
+    return "blue";
   if (percent < 0.8)
-    return "#60FF83";
+    return "light-green";
   if (percent < 0.95)
-    return "#FFA41B";
-  return "#FF3B36";
+    return "orange";
+  return "red";
 }
 
 function calculate_and_set_fill_bar() {
   chrome.storage.sync.get('clipboard', function(syncItems) {
     if (!(syncItems.clipboard instanceof Array))
       return;
+
+    $("#option_storage_usage").empty();
+    $("#option_storage_usage").append(chrome.i18n.getMessage("optionStorageUsage"));
+
     var size = computeObjectSize(syncItems.clipboard);
     var percentage = size/SYNC_QUOTA;
-    set_synchronized_fill_bar(percentage);
+    document.getElementById('progress_progress').style.width = Math.floor(percentage*100)+"%";
+    $('#progress_progress').addClass(getStorageColorClass(percentage));
+    $('#option_storage_usage').append(Math.floor(percentage*100)+"%");
   });
 }
 
@@ -76,40 +80,26 @@ function computeObjectSize(object) {
   return JSON.stringify(object).length;
 }
 
-function set_synchronized_fill_bar(percent) {
-  var fillBar = document.getElementById("option_storage_fill_synchronized");
-  var fillBarBackground = document.createElement("div");
-  fillBarBackground.className = "fill_bar_background";
-  fillBarBackground.style.backgroundColor = get_storage_fill_color(percent);
-  fillBarBackground.style.width = percent * fillBar.offsetWidth - 2;
-  fillBarBackground.style.height = fillBar.offsetHeight - 2;
-
-  var fillBarPercentage = document.getElementById("options_storage_percentage_fill");
-  fillBarPercentage.innerText = ": " + (Math.floor(percent*100)) + "%";
-
-  fillBar.insertBefore(fillBarBackground, fillBar.firstChild);
-}
-
 function init_i18n() {
-  document.getElementById("title").innerHTML = chrome.i18n.getMessage("optionsText");
-  document.getElementById("option_sync_type_text").innerHTML = chrome.i18n.getMessage("optionStorageTypeText");
-  document.getElementById("option_sync_text").innerHTML = chrome.i18n.getMessage("optionSyncText");
-  document.getElementById("option_swap_storages_text").innerHTML = chrome.i18n.getMessage("optionSwapStorageText");
-  document.getElementById("option_swap_storage_btn_text").innerText = chrome.i18n.getMessage("optionSwapText");
-  document.getElementById("option_local_text").innerHTML = chrome.i18n.getMessage("optionLocalText");
-  document.getElementById("option_sync_tip").innerText = chrome.i18n.getMessage("optionSyncTip");
-  document.getElementById("option_storage_usage").innerText = chrome.i18n.getMessage("optionStorageUsage");
-  document.getElementById("option_storage_name_synchronized").innerText = chrome.i18n.getMessage("optionSyncText");
+  $("#title").append(chrome.i18n.getMessage("optionsText"));
+  $("#option_sync_type_text").append(chrome.i18n.getMessage("optionStorageTypeText"));
+  $("#option_sync_text").html(chrome.i18n.getMessage("optionSyncText"));
+  $("#option_swap_storages_text").append(chrome.i18n.getMessage("optionSwapStorageText"));
+  $("#option_swap_storage_btn_text").text(chrome.i18n.getMessage("optionSwapText"));
+  $("#option_local_text").html(chrome.i18n.getMessage("optionLocalText"));
+  $("#option_sync_tip").append(chrome.i18n.getMessage("optionSyncTip"));
 
   document.getElementById("save").innerText = chrome.i18n.getMessage("optionsSave");
 }
 
-function dom_loaded() {
+$(document).ready(function() {
   init_i18n();
   restore_options();
+  $('select').material_select();
+  $('.modal').modal();
   calculate_and_set_fill_bar();
-}
+  document.getElementById("save").onclick = save_options;
+  document.querySelector('#option_swap_storage_btn_text').addEventListener('click', swap_storage);
+});
 
-document.addEventListener('DOMContentLoaded', dom_loaded);
-document.querySelector("#save").addEventListener('click', save_options);
-document.querySelector('#option_swap_storage_btn_text').addEventListener('click', swap_storage);
+
